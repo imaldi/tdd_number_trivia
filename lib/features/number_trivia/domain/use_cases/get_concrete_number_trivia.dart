4322eecd2434812874269da616993fbadee045c0
imaldi <aldiirsanmajid@gmail.com>
2022-01-23 02:50:39 +0700

    masih eror di "any"

---
+++ lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
@@ -0,0 +1,17 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:tdd_number_trivia/core/error/failures.dart';
+import 'package:tdd_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tdd_number_trivia/features/number_trivia/domain/repositories_contracts/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
